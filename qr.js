59f8ae5c780e9d97bb7fb858b5cadf2f6801d032
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-06-01 12:41:07 +0200

    Change overview

--- qr.js
+++ qr.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', function() {
 	const qr = document.getElementById('qr'),
 		input = document.getElementById('text'),
-		codeSelectors = document.getElementsByName('codeSelectRadio');
+		codeSelectors = document.getElementsByName('codeSelectRadio'),
+		pages = document.getElementsByClassName('page'),
+		left = document.getElementById('left'),
+		right = document.getElementById('right');
+	var page = 0;
 
 	function getSelectedCode(){
 		for(var i = 0; i < codeSelectors.length; i++){
@@ -47,4 +51,18 @@ window.addEventListener('load', function() {
 	document.getElementById('btn-gen').addEventListener('click', function() {
 		if (input.value.length) generate(input.value);
 	}, false);
+	left.addEventListener('click', function(e) {
+		right.style.removeProperty('color');
+		pages[page].style.display = 'none';
+		page--;
+		pages[page].style.removeProperty('display');
+		if (page === 0) e.target.style.color = 'transparent';
+	});
+	right.addEventListener('click', function(e) {
+		left.style.removeProperty('color');
+		pages[page].style.display = 'none';
+		page++;
+		pages[page].style.removeProperty('display');
+		if (page+1 === pages.length) e.target.style.color = 'transparent';
+	});
 }, false);
